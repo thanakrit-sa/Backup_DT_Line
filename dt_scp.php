ad0ac209077cb374cbf04d42f7d27d7012737e6d
thanakrit <thanakrit.10197@gmail.com>
2020-05-20 18:49:20 +0700

    now

--- dt_scp.php
+++ dt_scp.php
@@ -103,9 +103,9 @@ foreach ($events['events'] as $event) {
                 ];
             } else if ($bet_string == "สมัคร") {
                 $data = array(
-                    "user_displayname" => "$user_displayname",
-                    "fullname" => "$user_displayname",
-                    "user_lineid" => "$userID",
+                    "user_displayname" => $user_displayname,
+                    "fullname" => $user_displayname,
+                    "user_lineid" => $userID,
                 );
                 $data_string = json_encode($data);
 
