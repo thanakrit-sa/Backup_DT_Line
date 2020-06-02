00ae4cd2b9223068a5be3f8b4f124224911fbc66
thanakrit <thanakrit.10197@gmail.com>
2020-06-02 17:14:48 +0700

    now

--- dt_scp.php
+++ dt_scp.php
@@ -204,16 +204,16 @@ foreach ($events['events'] as $event) {
                         ]
                     ];
                 } else {
-                    $ch = curl_init('http://e-sport.in.th/ssdev/dt/dashboard/api/user_test/profile/' . $userID);
-                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',));
-                    $result = curl_exec($ch);
-                    curl_close($ch);
-                    $resultData = json_decode($result, true);
-                    $data = $resultData['data'];
-                    $user = $data['id'];
-                    $credit = $data['credit'];
+                    // $ch = curl_init('http://e-sport.in.th/ssdev/dt/dashboard/api/user_test/profile/' . $userID);
+                    // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',));
+                    // $result = curl_exec($ch);
+                    // curl_close($ch);
+                    // $resultData = json_decode($result, true);
+                    // $data = $resultData['data'];
+                    // $user = $data['id'];
+                    // $credit = $data['credit'];
                     $data = array(
                         "user_id" => "a",
                         "user_lineid" => "a",
