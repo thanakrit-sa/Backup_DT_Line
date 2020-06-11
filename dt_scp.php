52774cfe21da08ed68dc729be005aab18a469b5e
thanakrit <thanakrit.10197@gmail.com>
2020-06-11 18:23:55 +0700

    now

--- dt_scp.php
+++ dt_scp.php
@@ -284,12 +284,12 @@ foreach ($events['events'] as $event) {
                         if (strpos($text,"คี่") || strpos($text,"คู่") || strpos($text,"แดง") || strpos($text,"ดำ")) {
                             $messages = [
                                 'type' => 'text',
-                                'text' => "ชื่อผู้ใช้งาน : " . $user_displayname . "\r\n" . "เดิมพัน : " . $bet_text . "\r\n" . "จำนวน : " . $bet_value . " บาท" . "\r\n" . "รหัสเดิมพัน : " . $bet_code
+                                'text' => "ชื่อผู้ใช้งาน : "
                             ];
                         } else {
                             $messages = [
                                 'type' => 'text',
-                                'text' => "ชื่อผู้ใช้งาน : "
+                                'text' => "ชื่อผู้ใช้งาน : " . $user_displayname . "\r\n" . "เดิมพัน : " . $bet_text . "\r\n" . "จำนวน : " . $bet_value . " บาท" . "\r\n" . "รหัสเดิมพัน : " . $bet_code
                             ];
                         }
                     }
